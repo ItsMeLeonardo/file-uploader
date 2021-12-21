96b104de31f7a9bed3b076fd599f4739181fa0a8
Carlos Leonardao <74982565+ItsMeLeonardo@users.noreply.github.com>
2021-12-21 13:55:44 -0500

    [ADD] structure and some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,66 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <section className="Section-grid">
+      <header className="Logo">UpCloud</header>
+      <form className="layout Drop-zone">
+        <div className="Drop-content">
+          <h2 className="Drop-text">Drag and drop your file here</h2>
+
+          <p className="Drop-divider">
+            <span>or</span>
+          </p>
+
+          <label htmlFor="file" className="Drop-label btn btn-gradient">
+            Browse file
+            <input type="file" name="file" id="file" hidden />
+          </label>
+        </div>
+      </form>
+
+      <aside className="layout FilterableFiles-content">
+        <header className="FilterableFiles-header">
+          <span className="FilterableFiles-text">Files</span>
+          <button className="btn btn-text">Cancel all</button>
+        </header>
+        <ul className="Filters">
+          <li className="Filter-item active">
+            <i className="Filter-icon">icon</i>
+            <span className="Filter-text">All</span>
+          </li>
+          <li className="Filter-item">
+            <i className="Filter-icon">icon</i>
+            <span className="Filter-text">All</span>
+          </li>
+        </ul>
+
+        <ul className="Files">
+          <li className="File-item">
+            <div className="File-info">
+              <h2 className="File-text">File name</h2>
+              <span className="File-progress">80%</span>
+              <button className="File-detail btn btn-text">Details</button>
+              <i className="File-icon">icon</i>
+            </div>
+            <div className="progress-bar">
+              <span className="bar" />
+            </div>
+          </li>
+          <li className="File-item completed">
+            <div className="File-info">
+              <h2 className="File-text">File name</h2>
+              <span className="File-progress">80%</span>
+              <button className="File-detail btn btn-text">Details</button>
+              <i className="File-icon">icon</i>
+            </div>
+            <div className="progress-bar">
+              <span className="bar" />
+            </div>
+          </li>
+        </ul>
+      </aside>
+    </section>
   )
 }
 
