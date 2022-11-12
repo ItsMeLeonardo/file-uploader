8aa5b1ca731032816311ce042de39429a8f46ae3
itsmeleonardo <carlosgamercovi20@gmail.com>
2022-11-12 09:40:42 -0500

    fix color from icons

--- src/components/icons/IconClose.jsx
+++ src/components/icons/IconClose.jsx
@@ -12,7 +12,7 @@ function IconClose({ className, color = 'black', size = 20 } = {}) {
     >
       <path
         d="M9.88661 9.88661L6.11328 6.11328 M6.11328 9.88661L9.88661 6.11328 M7.99992 14.6667C11.6666 14.6667 14.6666 11.6667 14.6666 8.00004C14.6666 4.33337 11.6666 1.33337 7.99992 1.33337C4.33325 1.33337 1.33325 4.33337 1.33325 8.00004C1.33325 11.6667 4.33325 14.6667 7.99992 14.6667Z"
-        stroke={color}
+        stroke="currentColor"
         strokeWidth={1.5}
         strokeLinecap="round"
         strokeLinejoin="round"
--- src/components/icons/IconDocument.jsx
+++ src/components/icons/IconDocument.jsx
@@ -12,7 +12,7 @@ function IconDocument({ className, color = 'black' } = {}) {
     >
       <path
         d="M17.5 5.83329V14.1666C17.5 16.6666 16.25 18.3333 13.3333 18.3333H6.66667C3.75 18.3333 2.5 16.6666 2.5 14.1666V5.83329C2.5 3.33329 3.75 1.66663 6.66667 1.66663H13.3333C16.25 1.66663 17.5 3.33329 17.5 5.83329Z"
-        stroke={color}
+        stroke="currentColor"
         strokeWidth="1.5"
         strokeMiterlimit="10"
         strokeLinecap="round"
@@ -20,7 +20,7 @@ function IconDocument({ className, color = 'black' } = {}) {
       />
       <path
         d="M12.0833 3.75V5.41667C12.0833 6.33333 12.8333 7.08333 13.7499 7.08333H15.4166"
-        stroke={color}
+        stroke="currentColor"
         strokeWidth="1.5"
         strokeMiterlimit="10"
         strokeLinecap="round"
@@ -28,7 +28,7 @@ function IconDocument({ className, color = 'black' } = {}) {
       />
       <path
         d="M6.66675 10.8334H10.0001"
-        stroke={color}
+        stroke="currentColor"
         strokeWidth="1.5"
         strokeMiterlimit="10"
         strokeLinecap="round"
@@ -36,7 +36,7 @@ function IconDocument({ className, color = 'black' } = {}) {
       />
       <path
         d="M6.66675 14.1666H13.3334"
-        stroke={color}
+        stroke="currentColor"
         strokeWidth="1.5"
         strokeMiterlimit="10"
         strokeLinecap="round"
